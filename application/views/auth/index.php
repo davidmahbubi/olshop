2ff38a129a54d736977a1e9b871c8e11f88c701c
David Mahbubi <ulrichdavid0370@gmail.com>
2019-09-30 19:18:30 +0700

    added link to skip log in in auth login page

--- application/views/auth/index.php
+++ application/views/auth/index.php
@@ -26,6 +26,8 @@
 					<button type="submit" class="btn text-white dv-bg-primary bt-dv-bg-primary w-100 mb-3">Login</button>
 					<p class="mb-0">Don't have an account ? <a href="<?=base_url('auth/register')?>">here !</a></p>
 					<a href="#">Forgot your password ?</a>
+					<br>
+					<a href="<?= base_url() ?>">Skip log in</a>
 				</form>
 			</div>
 		</div>

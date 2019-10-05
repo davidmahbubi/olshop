dfba6892f2f6c516f3e028434a0f3748ec33d278
David Mahbubi <ulrichdavid0370@gmail.com>
2019-10-05 08:03:29 +0700

    Added print feature in product details

--- application/views/admin_product/details.php
+++ application/views/admin_product/details.php
@@ -4,13 +4,17 @@
 	<!-- Page Heading -->
 	<h1 class="h3 mb-4 text-gray-800"><a href="" onclick="history.go(-1); return false">
 			< Product Details</a> </h1> <div class="row">
-				<div class="col-lg-6 mb-3">
+            <div class="col-lg-3 mb-3 text-center">
+					<img width="350" src="<?=base_url()?>assets/img/product/<?=$product['img']?>" class="img-thumbnail"
+						alt="Missing Image File">
+				</div>  
+				<div class="col-lg-9 mb-3">
 					<ul class="list-group">
 						<li class="list-group-item"><b>Product Name : <?= $product['name'] ?></b></li>
 						<li class="list-group-item">Price : <?= formatPrice($product['price'], 'Rp') ?></li>
 						<li class="list-group-item">Category : <?= $category[$product['category_id']]['name'] ?></li>
-                        <li class="list-group-item">Stock : <?=$product['stock']?></li>
-                        <li class="list-group-item">Buy Times : <?=$buyTimes?> Times</li>
+						<li class="list-group-item">Stock : <?=$product['stock']?></li>
+						<li class="list-group-item">Buy Times : <?=$buyTimes?> Times</li>
 						<li class="list-group-item">Weight :
 							<?= $product['weight'] >= 1000  ? $product['weight']/1000 . " KG" : $product['weight'] . " grams"?>
 						</li>
@@ -26,10 +30,6 @@
 						</li>
 					</ul>
 				</div>
-				<div class="col-lg-6">
-					<img width="350" src="<?=base_url()?>assets/img/product/<?=$product['img']?>" class="img-thumbnail"
-						alt="Missing Image File">
-				</div>
 </div>
 <div class="row mt-3">
 	<div class="card shadow mb-4">
@@ -41,8 +41,25 @@
 		</div>
 	</div>
 </div>
+<button class="btn btn-primary btn-icon-split float-right" id="btPrint">
+	<span class="icon text-white-50">
+		<i class="fas fa-print"></i>
+	</span>
+	<span class="text">Print Product Details</span>
+</button>
+
 </div>
 <!-- /.container-fluid -->
 
 </div>
 <!-- End of Main Content -->
+
+<script>
+
+$(function(){
+    $('#btPrint').click(function(){
+        window.print();
+    })
+});
+
+</script>

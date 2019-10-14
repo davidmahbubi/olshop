c259504e26766dc44d02ba15509279ac28b2bd00
David Mahbubi <ulrichdavid0370@gmail.com>
2019-10-14 08:44:40 +0700

    fixed total input in product details

--- application/views/product/details.php
+++ application/views/product/details.php
@@ -3,8 +3,7 @@
 		< Back</a> <div class="row m-0">
 			<!-- Left -->
 			<div class="col-lg-4">
-				<img src="<?=base_url('assets/')?>img/product/<?=$product['img']?>"
-					class="img-thumbnail dv-details-prod-img mb-4">
+				<img src="<?= base_url('assets/') ?>img/product/<?= $product['img'] ?>" class="img-thumbnail dv-details-prod-img mb-4">
 			</div>
 			<!-- Right -->
 			<div class="col-lg-8">
@@ -14,17 +13,17 @@
 					<div class="col-lg-8 mb-3">
 						<ul class="list-group">
 							<li class="list-group-item">
-								<?php if($product['rating']) : ?>
-								<?php for($i = 0; $i < $product['rating']; $i++) : ?>
-								<i class="fas fa-star"></i>
-								<?php endfor; ?>
-								<?php else: ?>
-								<span>No rating yet</span>
+								<?php if ($product['rating']) : ?>
+									<?php for ($i = 0; $i < $product['rating']; $i++) : ?>
+										<i class="fas fa-star"></i>
+									<?php endfor; ?>
+								<?php else : ?>
+									<span>No rating yet</span>
 								<?php endif; ?>
 							</li>
 							<li class="list-group-item">
 								<i class="fas fa-fw fa-weight-hanging mr-2"></i>
-								<span><?=$product['weight'] >= 1000 ? convertToKg($product['weight']) : $product['weight'] . ' grams'?></span>
+								<span><?= $product['weight'] >= 1000 ? convertToKg($product['weight']) : $product['weight'] . ' grams' ?></span>
 							</li>
 							<li class="list-group-item">
 								<i class="fas fa-fw fa-calendar-day mr-2">
@@ -37,27 +36,24 @@
 							</li>
 							<li class="list-group-item">
 								<i class="fas fa-fw fa-tag mr-2"></i>
-								<a
-									href="<?=base_url()?>product?cat=<?= $product['category_id'] ?>"><?= $product['category_name']; ?></a>
+								<a href="<?= base_url() ?>product?cat=<?= $product['category_id'] ?>"><?= $product['category_name']; ?></a>
 							</li>
 						</ul>
 					</div>
 					<div class="col-lg-4">
-						<form action="<?=base_url()?>product/buy/<?=$product['id']?>" method="POST">
+						<form action="<?= base_url() ?>product/buy/<?= $product['id'] ?>" method="POST">
 							<div class="input-group mb-3">
 								<div class="input-group-prepend">
 									<span class="input-group-text" id="basic-addon1">Total : </span>
-                </div>
-                <input type="number" <?= $product['stock'] == 0? 'disabled':''?> min="1" name="qty" value="1" class="form-control bt-cart" id="inputCart"
-                placeholder="Username" aria-label="Username" aria-describedby="basic-addon1">
+								</div>
+								<input type="number" <?= $product['stock'] == 0 ? 'disabled' : '' ?> min="1" name="qty" value="1" class="form-control bt-cart" id="inputCart" placeholder="Buy Total" aria-label="Username" required>
 							</div>
-              <?php if(isLoggedIn()) : ?>
-                <button type="button" <?= $product['stock'] == 0? 'disabled':''  ?> class="btn w-100 btn-outline-primary mb-2 addToCart"><i
-                class="fas fa-cart-plus cart-ic" style="font-size: 30px;"></i></button>
-              <?php endif; ?>
-							<button type="submit" <?= $product['stock'] == 0? 'disabled':'' ?> class="btn text-white w-100 dv-bg-primary bt-dv-bg-primary">Buy</button>
+							<?php if (isLoggedIn()) : ?>
+								<button type="button" <?= $product['stock'] == 0 ? 'disabled' : ''  ?> id="addToCartBt" class="btn w-100 btn-outline-primary mb-2 addToCart"><i class="fas fa-cart-plus cart-ic" style="font-size: 30px;"></i></button>
+							<?php endif; ?>
+							<button type="submit" <?= $product['stock'] == 0 ? 'disabled' : '' ?> class="btn text-white w-100 dv-bg-primary bt-dv-bg-primary" id="buyBt">Buy</button>
 						</form>
-						<?=$product['stock'] == 0 ? '<small class="text-danger text-center" style="font-size: 15px;">This product is out of stock </small>' : ''?>
+						<?= $product['stock'] == 0 ? '<small class="text-danger text-center" style="font-size: 15px;">This product is out of stock </small>' : '' ?>
 					</div>
 				</div>
 			</div>
@@ -72,74 +68,84 @@
 				<div class="col-lg">
 					<h1>Review</h1>
 					<div class="row mt-3">
-					<?php if(!empty($review)) : ?>
-					<?php foreach($review as $r) : ?>
-						<div class="col-md mb-3 m-0">
-							<div class="card mb-3" style="max-width: 540px; ">
-								<div class="row no-gutters">
-									<div class="col-md-4">
-										<img src="<?=base_url()?>assets/img/profile/<?=$r['image']?>" width="200" class="card-img h-100">
-									</div>
-									<div class="col-md-8">
-										<div class="card-body">
-											<h5 class="card-title"><?= $r['first_name'] . " " . $r['last_name'] ?></h5>
-											<p class="card-text"><?= $r['review'] ?>
-											</p>
-											<p class="card-text"><small class="text-muted">
-											<?php for($i = 0; $i < $r['rating']; $i++): ?>
-												<i class="fas fa-star"></i>
-											<?php endfor; ?>
-												</small></p>
-											<small><?= date('d F Y', $r['date_posted']); ?></small>
+						<?php if (!empty($review)) : ?>
+							<?php foreach ($review as $r) : ?>
+								<div class="col-md mb-3 m-0">
+									<div class="card mb-3" style="max-width: 540px; ">
+										<div class="row no-gutters">
+											<div class="col-md-4">
+												<img src="<?= base_url() ?>assets/img/profile/<?= $r['image'] ?>" width="200" class="card-img h-100">
+											</div>
+											<div class="col-md-8">
+												<div class="card-body">
+													<h5 class="card-title"><?= $r['first_name'] . " " . $r['last_name'] ?></h5>
+													<p class="card-text"><?= $r['review'] ?>
+													</p>
+													<p class="card-text"><small class="text-muted">
+															<?php for ($i = 0; $i < $r['rating']; $i++) : ?>
+																<i class="fas fa-star"></i>
+															<?php endfor; ?>
+														</small></p>
+													<small><?= date('d F Y', $r['date_posted']); ?></small>
+												</div>
+											</div>
 										</div>
 									</div>
 								</div>
-							</div>
-						</div>
-						<?php endforeach; ?>
-					<?php else: ?>
-						<h5 class="ml-3">No review</h5>
-					<?php endif; ?>
+							<?php endforeach; ?>
+						<?php else : ?>
+							<h5 class="ml-3">No review</h5>
+						<?php endif; ?>
 					</div>
 				</div>
 			</div>
 </div>
 
 <script>
-	$(function () {
+	$(function() {
+
+		$('#inputCart').change(function() {
+			if ($(this).val() <= 0) {
+				console.log($(this));
+				$('#addToCartBt').addClass('disabled').attr('disabled', 'disabled');
+				$('#buyBt').addClass('disabled').attr('disabled', 'disabled');
+			} else {
+				$('#addToCartBt').removeClass('disabled').removeAttr('disabled', 'disabled');
+				$('#buyBt').removeClass('disabled').removeAttr('disabled', 'disabled');
+			}
+		});
 
 		// Hover function
 
-		$('.addToCart').hover(function () {
+		$('.addToCart').hover(function() {
 			$('.cart-ic').css('color', 'white');
-		}, function () {
+		}, function() {
 			$('.cart-ic').css('color', '#00adb5');
 		});
 
 		// Add to cart
 
-		$('.addToCart').click(function () {
+		$('.addToCart').click(function() {
 
 			let total = $('#inputCart').val();
 
 			$.ajax({
-				url: '<?=base_url()?>cart/addtocart',
+				url: '<?= base_url() ?>cart/addtocart',
 				data: {
 					total: total,
-					id: '<?=$product['id']?>',
-					price: '<?=$product['price']?>',
-					name: '<?=$product['name']?>',
-					image: '<?=$product['img']?>'
+					id: '<?= $product['id'] ?>',
+					price: '<?= $product['price'] ?>',
+					name: '<?= $product['name'] ?>',
+					image: '<?= $product['img'] ?>'
 				},
 				method: 'post',
 				dataType: 'json',
-				success: function (result) {
+				success: function(result) {
 					if (result.stats) {
-						alert('<?=$product['name']?> * ' + total + ' is added to yur cart !');
+						alert('<?= $product['name'] ?> * ' + total + ' is added to yur cart !');
 					}
 				}
 			});
 		});
 	});
-
 </script>
--- application/views/templates/front-end/header.php
+++ application/views/templates/front-end/header.php
@@ -15,12 +15,13 @@
       href="https://fonts.googleapis.com/css?family=Source+Sans+Pro&display=swap"
       rel="stylesheet"
     />
+
     <link
       rel="stylesheet"
       href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.10.2/css/all.min.css"
     />
 
-    <script src="https://www.google.com/recaptcha/api.js" async defer></script>
+    <script src="https://www.google.com/recaptcha/api.js?hl=en" async defer></script>
 
     <script src="<?=base_url('assets/')?>js/jquery-3.4.1.min.js"></script>
 
